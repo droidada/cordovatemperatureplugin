cc14b8e91cf9ecb7975cd3d86befd47edc565a6c
Adaobi Frank <adaobi.frank@moduscreate.com>
2018-08-06 14:50:23 +0100

    bug fix

--- www/Temperature.js
+++ www/Temperature.js
@@ -1,9 +1,9 @@
 var exec = require('cordova/exec');
 
 exports.checkTemperature = function(arg0, success, error) {
-    exec(success, error, 'TemperaturePlugin', 'checkTemperature', [arg0]);
+    exec(success, error, 'Temperature', 'checkTemperature', [arg0]);
 };
 
 exports.isDeviceCompatible = function(arg0, success, error) {
-    exec(success, error, 'TemperaturePlugin', 'isDeviceCompatible', [arg0]);
+    exec(success, error, 'Temperature', 'isDeviceCompatible', [arg0]);
 };
